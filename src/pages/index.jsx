b3bf7a5460b867811bfa64bcbce56b0f6829bc10
Rizky Alfiansyah <rizkkyaf@gmail.com>
2022-11-10 02:14:56 +0800

    responsive mobile

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     <div className="snap-y snap-mandatory w-screen h-screen overflow-auto overflow-x-hidden">
       {/* Hero section */}
-      <section className="w-full md:min-h-screen bg-hero bg-cover bg-no-repeat bg-center flex flex-col items-center justify-between md:gap-0 gap-20 lg:pt-16 pt-10">
+      <section className="w-full md:min-h-screen h-screen bg-hero bg-cover bg-no-repeat bg-center flex flex-col items-center justify-between md:gap-0 gap-20 lg:pt-16 pt-10">
         <Hero onClick={(value) => _handleRef(value)} />
       </section>
       {/* second Section */}
