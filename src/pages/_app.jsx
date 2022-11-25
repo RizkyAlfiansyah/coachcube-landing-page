9b360b057cfea6736fa3003e90ebc7af50a73d34
Rizky Alfiansyah <rizkkyaf@gmail.com>
2022-11-26 00:32:37 +0800

    add meta seo tag

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, photo }) {
 
   const [hydrated, setHydrated] = useState(false);
 
@@ -20,8 +20,16 @@ function MyApp({ Component, pageProps }) {
         <Head>
           <title>Coachcube</title>
           <meta charSet="UTF-8" />
-          <meta name="description" content="Coachcube" />
-          <meta name="og:title" property="og:title" content="Coachcube" />
+          <meta property="og:url" content="www.coach-cube.com" />
+          <meta property="og:type" content="website" />
+          <meta property="fb:app_id" content="your fb id" />
+          <meta property="og:title" content={photo?.title} />
+          <meta name="twitter:card" content="summary" />
+          <meta
+            property="og:description"
+            content="Hurray!! Yes Social Media Preview is Working"
+          />
+          <meta property="og:image" content={photo?.url} />
           <link rel="icon" href="/icon.png" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no" />
         </Head>
@@ -34,3 +42,18 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
+export const getServerSideProps = async () => {
+  let photo = null;
+  await fetch('https://jsonplaceholder.typicode.com/photos/1')
+    .then((response) => response.json())
+    .then((json) => {
+      photo = json
+    })
+
+  return {
+    props: {
+      photo,
+    },
+  };
+};
