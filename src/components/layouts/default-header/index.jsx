408d3293a70cfc637d110d9972d1c4514b9df220
Rizky Alfiansyah <rizkkyaf@gmail.com>
2022-11-26 01:06:53 +0800

    change ssr

--- src/components/layouts/default-header/index.jsx
+++ src/components/layouts/default-header/index.jsx
@@ -1,7 +1,20 @@
 import Head from 'next/head'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Header = ({ photo }) => {
+    const [photos, setPhotos] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await fetch('https://jsonplaceholder.typicode.com/photos/1')
+                .then((response) => response.json())
+                .then((json) => {
+                    setPhotos(json)
+                })
+        }
+        fetchData()
+    }, [])
+
     return (
         <Head>
             <title>Coachcube</title>
@@ -9,13 +22,13 @@ const Header = ({ photo }) => {
             <meta property="og:url" content="www.coach-cube.com" />
             <meta property="og:type" content="website" />
             <meta property="fb:app_id" content="your fb id" />
-            <meta property="og:title" content={photo?.title} />
+            <meta property="og:title" content={photos?.title} />
             <meta name="twitter:card" content="summary" />
             <meta
                 property="og:description"
                 content="Hurray!! Yes Social Media Preview is Working"
             />
-            <meta property="og:image" content={photo?.url} />
+            <meta property="og:image" content={photos?.url} />
             <link rel="icon" href="/icon.png" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no" />
         </Head>
@@ -23,19 +36,3 @@ const Header = ({ photo }) => {
 }
 
 export default Header;
-
-
-export const getServerSideProps = async () => {
-    let photo = null;
-    await fetch('https://jsonplaceholder.typicode.com/photos/1')
-        .then((response) => response.json())
-        .then((json) => {
-            photo = json
-        })
-
-    return {
-        props: {
-            photo,
-        },
-    };
-};
